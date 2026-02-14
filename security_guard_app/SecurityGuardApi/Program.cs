e1888822ad2e4bc69e09535766787606d6365bad
Saqib <Saqibmaklai@gmail.com>
2026-02-13 18:56:49 -0600

    Use default PostgreSQL port 5432 when not specified in URI

--- security_guard_app/SecurityGuardApi/Program.cs
+++ security_guard_app/SecurityGuardApi/Program.cs
@@ -50,7 +50,8 @@ if (isPostgreSQL)
     {
         var uri = new Uri(connectionString!);
         var userInfo = uri.UserInfo.Split(':');
-        connectionString = $"Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
+        var port = uri.Port > 0 ? uri.Port : 5432; // Default PostgreSQL port
+        connectionString = $"Host={uri.Host};Port={port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
         Console.WriteLine($"Converted to key-value format successfully");
     }
     catch (Exception ex)
